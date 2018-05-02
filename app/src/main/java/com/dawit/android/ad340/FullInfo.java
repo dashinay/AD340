5ae6197e1768ae66dcec756b8ca5bb54537393ce
dawit <dawit42001@gmail.com>
2018-05-02 03:14:30 -0700

    Revert "Revert "Revert ""HW 4""""
    This reverts commit e5b59962803368c26ded7f662602032b4e29d9b2.

--- app/src/main/java/com/dawit/android/ad340/FullInfo.java
+++ app/src/main/java/com/dawit/android/ad340/FullInfo.java
@@ -2,17 +2,11 @@ package com.dawit.android.ad340;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
 
 public class FullInfo extends AppCompatActivity {
     RecyclerView recyclerView;
@@ -41,28 +35,5 @@ public class FullInfo extends AppCompatActivity {
 
         recyclerView.setAdapter(recyclerViewAdapter);
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
-        ActionBar actionbar = getSupportActionBar();
-        actionbar.setTitle("Full Movie Description");
-        actionbar.setDisplayHomeAsUpEnabled(true);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.settings_menu:
-                Toast.makeText(getApplicationContext(), "You clicked the setting button.", Toast.LENGTH_SHORT).show();
-                break;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.menu, menu);
-        return true;
     }
 }
