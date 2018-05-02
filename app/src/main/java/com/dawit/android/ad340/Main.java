e5b59962803368c26ded7f662602032b4e29d9b2
dawit <dawit42001@gmail.com>
2018-05-02 03:13:45 -0700

    Revert "Revert ""HW 4"""
    This reverts commit 67b3d0dfdfebe2f438df6fae60d14e5f5256c1cf.

--- app/src/main/java/com/dawit/android/ad340/Main.java
+++ app/src/main/java/com/dawit/android/ad340/Main.java
@@ -1,33 +1,92 @@
 package com.dawit.android.ad340;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class Main extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.dawit.android.ad340.MESSAGE";
+
+    DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setTitle("AD340 Application");
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        switch (menuItem.getItemId()){
+                            case R.id.nav_movies:
+                                Intent intent= new Intent(getApplicationContext(), MovieList.class);
+                                startActivity(intent);
+                                break;
+                            case R.id.nav_about:
+                                Intent intent2= new Intent(getApplicationContext(), About.class);
+                                startActivity(intent2);
+                                break;
+                        }
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24px);
     }
 
-    public void leftTop(View view){
-        Intent intent = new Intent(this, MovieList.class);
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 
-    public void rightTop(View view){
-        Toast.makeText(getApplicationContext(), "Right top button.", Toast.LENGTH_SHORT).show();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            case R.id.settings_menu:
+                Toast.makeText(getApplicationContext(), "You clicked the setting button.", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
-    public void leftBottom(View view){
-        Toast.makeText(getApplicationContext(), "Left bottom button.", Toast.LENGTH_SHORT).show();
-    }
-
-    public void rightBottom(View view){
-        Toast.makeText(getApplicationContext(), "right bottom button.", Toast.LENGTH_SHORT).show();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
     }
 }
